1b8bd4a88b30f2fdab9adeb79ef548068b2ec220
catsucci <150689455+catsucci@users.noreply.github.com>
2024-09-15 14:57:29 +0100

    backup: gonna switch the OS

--- src/App.js
+++ src/App.js
@@ -7,4 +7,61 @@
  * Created: 2024-09-14
  */
 
-console.log("Hello, World!");
+function GameBoard() {
+  const rows = 3;
+  const columns = 3;
+  const board = [];
+
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board[i][j] = Cell({ x: j, y: i });
+    }
+  }
+
+  const GetBoard = () => board;
+
+  const GetCell = ({ x, y }) => {
+    if (x != -1 && y != -1 && x < columns && y < rows) {
+      return board[y][x];
+    } else {
+      console.log(
+        `OUT OF SCOOP ERROR: INVALID COORDINATES\n\t(${x}, ${y}) ARE INVALID AS THEY ARE OUTSIDE THE SCOOP OF THE GAME BOARD.`
+      );
+    }
+  };
+
+  const PlaceSymbol = (coordinates, player) => {
+    const cell = GetCell(coordinates);
+  };
+
+  console.log({ board: board });
+
+  return {
+    GetBoard,
+    PlaceSymbol,
+    GetCell,
+  };
+}
+
+function Cell({ x, y }) {
+  let symbol = null;
+  const coordinates = {
+    x: x,
+    y: y,
+  };
+
+  const AddSymbol = (player) => (symbol = player);
+
+  const GetSymbol = () => symbol;
+
+  const GetCoordinates = () => coordinates;
+
+  return {
+    AddSymbol,
+    GetSymbol,
+    GetCoordinates,
+  };
+}
+
+GameBoard();
